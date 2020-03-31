9daf267f76638ff8b681df5b05d32381943d9dc4
Jaroslav Kvas <xkvasn14@vutbr.cz>
2020-03-31 21:34:17 +0200

    0.0.2
    System.Math finish
    Ready to go :)

--- Calculator - material/Math/Math.cs
+++ Calculator - material/Math/Math.cs
@@ -28,14 +28,19 @@ namespace Calculator___material.Math
 
         public double Div(double divident, double divisor)
         {
-            if (divisor == 0)
+            if (divisor == 0 && divident > 0)
                 return Double.PositiveInfinity;
-            else
-                return (divident / divisor);
+            if (divisor == 0 && divident < 0)
+                return Double.NegativeInfinity;
+            if (divisor == 0 && divident == 0)
+                return Double.NaN;
+            return (divident / divisor);
         }
 
         public double Mod(double divident, double divisor)
         {
+            if (divisor == 0)
+                return Double.NaN;
             return (divident % divisor);
         }
 
@@ -57,17 +62,18 @@ namespace Calculator___material.Math
             }
         }
 
-        public double Fct(double factorial)
+        public double Fac(double factorial)
         {
             if ((factorial % 1) != 0)
                 return Double.NaN;
             if (factorial < 0)
                 return Double.NaN;
 
-            for (int i = Convert.ToInt32(factorial); i > 0; i--)
-                factorial = factorial * (factorial - i);
             if (factorial == 0)
                 return 1.0;
+
+            for (int i = Convert.ToInt32(factorial); i > 0; i--)
+                factorial = factorial * (factorial - i);
             return Convert.ToDouble(factorial);
         }
     }
